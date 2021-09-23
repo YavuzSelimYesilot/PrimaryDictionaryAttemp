5af5c4d9a7dfe71f72e94337ddee3907e1f2da29
Yavuz Selim <yavuz.yesilot@gmail.com>
2021-09-23 17:04:00 +0300

    Add project files.

---
+++ PrimaryDictionaryAttemp/MyDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimaryDictionaryAttemp
+{
+    class MyDictionary<T, K>
+    {
+        //2 değer tutan bir yapı     (key, value)
+
+        //Example
+        //Dictionary<string, string> openWith = new Dictionary<string, string>();
+        //openWith.Add("txt", "notepad.exe");
+
+        T[] key;
+        K[] value;
+
+        public MyDictionary()
+        {
+            key = new T[0];
+            value = new K[0];
+        }
+
+        public void Add(T item1, K item2)
+        {
+            T[] tempKeyArray = key;
+            key = new T[key.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                key[i] = tempKeyArray[i];
+            }
+
+            key[key.Length - 1] = item1;
+
+            //-----------------------------------
+
+            K[] tempValueArray = value;
+            value = new K[value.Length + 1];
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                value[i] = tempValueArray[i];
+            }
+
+            value[value.Length - 1] = item2;
+
+        }
+
+        public int Length
+        {
+            get { return key.Length; }
+        }
+
+        public  T[] Keys
+        {
+            get { return key; }
+        }
+
+        public K[] Values
+        {
+            get { return value; }
+        }
+
+
+    }
+}
---
+++ PrimaryDictionaryAttemp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrimaryDictionaryAttemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> nameAge = new MyDictionary<string, int>();
+
+            nameAge.Add("Ali", 54);
+            nameAge.Add("Veli", 12);
+            nameAge.Add("Hakkı", 71);
+            nameAge.Add("Osman", 26);
+
+            for (int i = 0; i < nameAge.Keys.Length; i++)
+            {
+                Console.Write("["+nameAge.Keys[i] + ", ");
+                Console.Write(nameAge.Values[i] + "]");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Eleman Sayısı: " + nameAge.Length);
+
+
+
+
+
+
+
+        }
+    }
+}
